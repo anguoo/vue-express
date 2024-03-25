03dadbd5da7adadafdb556a18a11f53181e807d8
ppo-0 <1304578864@qq.com>
2024-03-25 16:59:41 +0800

    feat:新增JWT认证机制

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
+const jwt_access = require('jsonwebtoken')
+const jwt_refresh = require('jsonwebtoken')
+const passport = require('passport')
+const secret = require('../config/mongodb').secretOrKey
 
 router.get("/test", (req, res) => {
     res.json({msg: "login works"})
@@ -15,12 +19,32 @@ router.post("/login", (req, res) => {
                 return res.status(404).json({msg:"用户不存在"})
             }
             if (user.passWord === password) {
-                res.json({msg: "success"})
+                const rlue = {id: user.id, role: user.role}
+                // jwt.sign("规则", "加密名字", "过期时间", "箭头函数")
+                let accessToken, refreshToken
+                jwt.sign(rlue, secret, {expiresIn: 3600}, (err, token) => {
+                    if (err) throw err
+                    accessToken = token
+                })
+                const longTime = 3600 * 24 * 30
+                jwt_refresh.sign(rlue, secret, {expiresIn: longTime}, (err, token) => {
+                    if (err) throw err
+                    refreshToken = token
+                    res.json({
+                        success: true,
+                        accessToken: "Bearer " + accessToken,
+                        refreshToken: "Bearer " + refreshToken,
+                    })
+                })
             } 
             else {
                 res.status(400).json({msg: "密码错误"})
             }
         })
+})
+
+router.get("/token", passport.authenticate("jwt", {session: false}), (req, res) => {
+    res.json({msg: "success"})
 })
 
 module.exports = router
