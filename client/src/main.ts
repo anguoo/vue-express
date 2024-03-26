6a4f1f41eb047186f126671a49b42c9a0d91c026
ppo-0 <1304578864@qq.com>
2024-03-26 15:01:01 +0800

    feat:新增登录页面

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,8 +3,13 @@ import router from './router'
 import { createApp } from 'vue'
 import 'element-plus/dist/index.css'
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 app.use(ElementPlus)
+app.use(router)
 app.mount('#app')
