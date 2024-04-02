85c0fa6693f2525f1b1d1c2ca04a94d30224c597
angu_o <127409900+anguoo@users.noreply.github.com>
2024-04-02 17:02:53 +0800

    feat:新增首页和用户管理页面 (#9)

---
+++ client/src/components/cheader.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="header">
+        <div class="right">
+            <div class="user">
+                <el-avatar 
+                    :size="45" 
+                    src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" 
+                    class="avatar"
+                />
+                <span class="user-name">admin</span>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+</script>
+<style scoped lang="css">
+.header {
+    width: 100%;
+    height: 50px;
+    padding: 0 10px;
+    background-color: #fff;
+    box-shadow: 0 1px 3px rgba(26,26,26,.1);
+
+    .right {
+        height: 100%;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        
+        .user {
+            display: flex;
+            align-items: center;
+
+            .avatar {
+                width: 35px;
+                height: 35px;
+                border-radius: 50%;
+                margin: 0 10px 0 30px;
+            }
+
+            .user-name {
+                font-size: 16px;
+                font-weight: 500;
+            }
+        }
+    }
+}
+</style>
---
+++ client/src/views/user/user.vue
@@ -0,0 +1,133 @@
+<template>
+    <div class="user-box">
+        <Nav navActive="/user"></Nav>
+        <div class="user-contents-box">
+            <CHeader></CHeader>
+            <section class="section">
+                <div class="head">
+                    <span>用户信息</span>
+                    <el-input
+                        v-model="searchContents"
+                        style="width: 220px"
+                        size="small"
+                        placeholder="Please Input"
+                        :prefix-icon="Search"
+                        />
+                </div>
+                <div class="show">
+                    <span class="avatar">头像</span>
+                    <span class="name">昵称</span>
+                    <span class="operate">操作</span>
+                </div> 
+                <div class="user-list">
+                    <div class="user">
+                        <div class="avatar">
+                            <el-avatar 
+                                src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+                            />
+                        </div>
+                        <div class="name">admin</div>
+                        <div class="operates">
+                            <button class="detail">详情</button>
+                            <button class="banned">封禁</button>
+                            <button class="reset">重置密码</button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
+<script setup lang="ts">
+import Nav from '@/components/nav.vue'
+import CHeader from '@/components/cheader.vue'
+import { Search } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+
+const searchContents = ref('')
+</script>
+<style scoped lang="css">
+.user-box {
+    width: 100%;
+    height: 100%;
+    background-color: #eeeeee;
+    display: flex;
+
+    .user-contents-box {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+
+        .section {
+            flex: 1;
+            padding: 0 10px;
+            margin-top: 10px;
+
+            .head {
+                width: 100%;
+                padding: 15px;
+                background-color: #fff;
+                border-bottom: 1px solid #eeeeee;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+            }
+            .show {
+                width: 100%;
+                background-color: #ffffffb2;
+                padding: 15px;
+                border-bottom: 1px solid #eeeeee;
+                display: flex;
+
+                .avatar,
+                .name {
+                    display: block;
+                    width: 100px;
+                }
+            }
+
+            .user-list {
+                width: 100%;
+                padding: 10px 15px;
+                background-color: #fff;
+                border-bottom: 1px solid #eeeeee;
+
+                .user {
+                    width: 100%;
+                    display: flex;
+                    align-items: center;
+
+                    .avatar {
+                        width: 100px;
+                    }
+                    .name {
+                        width: 95px;
+                    }
+
+                    .operates {
+                        .detail,
+                        .banned,
+                        .reset {
+                            margin-right: 10px;
+                            background-color: #fff;
+                            outline: none;
+                            border: none;
+                        }
+
+                        .detail {
+                            color: #525252;
+                        }
+
+                        .banned {
+                            color: red;
+                        }
+                        .reset {
+                            color: rgb(0, 145, 255);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
